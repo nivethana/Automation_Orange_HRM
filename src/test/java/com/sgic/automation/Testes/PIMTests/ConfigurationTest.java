4826c0e4eab4cb8b3543341a087c8cf935e1ae1b
abi1994 <abisel99@gmail.com>
2019-07-30 17:50:21 +0530

    first

---
+++ src/test/java/com/sgic/automation/Testes/PIMTests/ConfigurationTest.java
@@ -0,0 +1,15 @@
+package com.sgic.automation.Testes.PIMTests;
+
+import com.sgic.automation.Utils.TestBase;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class ConfigurationTest extends TestBase {
+    @Test(priority = 1)
+    public void optioalFiles(){
+    softAssert = new SoftAssert();
+
+    }
+
+
+}
